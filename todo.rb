d302a0cdab9ccb3f68375c683489d11f46cecca5
vonnenaut <danielashcom@gmail.com>
2020-04-14 11:34:02 +0700

    u/d todo.rb, layout.erb and added list.erb, edit_list.erb, lists.erb and new_list.erb

--- todo.rb
+++ todo.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+require "sinatra/content_for"
 
 configure do
   enable :sessions
@@ -26,6 +27,13 @@ get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# renders the edit list form
+get "/lists/:id/edit" do
+  list_id = params[:id].to_i
+  @name = session[:lists][list_id]
+  erb :edit_list, layout: :layout
+end
+
 # Returns an error message if the name is invalid (wrong length or already in use); otherwise returns nil if valid.
 def error_for_list_name(name)
   if !(1..100).cover? name.size
@@ -39,7 +47,8 @@ end
 post "/lists" do
   list_name = params[:list_name].strip
   
-  if error = error_for_list_name(list_name)
+  error = error_for_list_name(list_name)
+  if error
     session[:error] = error
     erb :new_list, layout: :layout
   else
@@ -48,3 +57,17 @@ post "/lists" do
     redirect "/lists"
   end
 end
+
+# Views a specific list and its tasks
+get "/lists/:id" do
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+
+  if @list_id > session[:lists].length - 1
+    session[:error] = "That To-Do list doesn't exist."
+    redirect "/lists"
+  else
+    erb :list, layout: :layout
+  end
+end
+
